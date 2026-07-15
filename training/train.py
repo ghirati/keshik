6ce7261bfe7b43ddb750c2c08a3033880bdae92d
Mohammad Mehdi Ghirati <mohammad.ghirati@proton.me>
2026-07-15 11:32:14 +0330

    Format train.py per PEP8

--- training/train.py
+++ training/train.py
@@ -42,8 +42,10 @@ def train(model, train_loader, val_loader, device, num_epochs, learning_rate, l2
     loss_fn = nn.BCEWithLogitsLoss()
 
     # Values are adapted from Wake Vision paper.
-    optimizer = optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=l2_value)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=num_epochs)
+    optimizer = optim.AdamW(
+        model.parameters(), lr=learning_rate, weight_decay=l2_value)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(
+        optimizer, T_max=num_epochs)
 
     train_losses = []
     val_losses = []
@@ -53,14 +55,16 @@ def train(model, train_loader, val_loader, device, num_epochs, learning_rate, l2
 
     csv_path = f"{checkpoint_dir}/metrics.csv"
     with open(csv_path, "w", newline="") as f:
-        csv.writer(f).writerow(["epoch", "train_loss", "train_acc", "val_loss", "val_acc", "val_precision", "val_recall", "val_f1"])
+        csv.writer(f).writerow(["epoch", "train_loss", "train_acc",
+                                "val_loss", "val_acc", "val_precision", "val_recall", "val_f1"])
 
     for epoch in range(num_epochs):
         model.train()
         train_loss = 0.0
         tp = fp = tn = fn = 0
 
-        progress_bar = tqdm(train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False)
+        progress_bar = tqdm(
+            train_loader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False)
         for imgs, y_actual in progress_bar:
             imgs, y_actual = imgs.to(device), y_actual.to(device)
             y_pred = model(imgs)
@@ -83,19 +87,22 @@ def train(model, train_loader, val_loader, device, num_epochs, learning_rate, l2
         train_acc = (tp+tn) / (tp+fp+tn+fn)
         avg_train_loss = train_loss / len(train_loader)
         train_losses.append(avg_train_loss)
-        tqdm.write(f"Epoch {epoch+1}\nTrain -> Loss: {avg_train_loss:.4f}, TrainAcc: {train_acc:.4f}")
+        tqdm.write(
+            f"Epoch {epoch+1}\nTrain -> Loss: {avg_train_loss:.4f}, TrainAcc: {train_acc:.4f}")
 
-        avg_val_loss, accuracy, precision, recall, f1 = validate(model, val_loader, loss_fn, device, threshold)
+        avg_val_loss, accuracy, precision, recall, f1 = validate(
+            model, val_loader, loss_fn, device, threshold)
         val_losses.append(avg_val_loss)
         val_accuracies.append(accuracy)
         val_precisions.append(precision)
         val_recalls.append(recall)
         val_f1s.append(f1)
         with open(csv_path, "a", newline="") as f:
-            csv.writer(f).writerow([epoch+1, avg_train_loss, train_acc, avg_val_loss,
-                            accuracy, precision, recall, f1])
+            csv.writer(f).writerow(
+                [epoch+1, avg_train_loss, train_acc, avg_val_loss, accuracy, precision, recall, f1])
 
-        least_val_loss = save_checkpoint(model, avg_val_loss, least_val_loss, checkpoint_dir, epoch)
+        least_val_loss = save_checkpoint(
+            model, avg_val_loss, least_val_loss, checkpoint_dir, epoch)
 
     return model, train_losses, val_losses, val_accuracies, val_precisions, val_recalls, val_f1s
 
@@ -104,8 +111,10 @@ def validate(model, val_loader, loss_fn, device, threshold):
     model.eval()
     val_loss = 0.0
     tp = fp = tn = fn = 0
+
+    progress_bar = tqdm(val_loader, desc="Validation", leave=False)
     with torch.no_grad():
-        for imgs, y_actual in val_loader:
+        for imgs, y_actual in progress_bar:
             imgs, y_actual = imgs.to(device), y_actual.to(device)
             y_pred = model(imgs)
             loss = loss_fn(y_pred.squeeze(1), y_actual.float())
@@ -119,20 +128,23 @@ def validate(model, val_loader, loss_fn, device, threshold):
             fn += ((preds == 0) & (y_actual == 1)).sum().item()
             val_loss += loss.item()
 
-            accuracy = (tp+tn) / (tp+fp+tn+fn)
-            precision = tp / (tp+fp) if (tp+fp) else 0.0
-            recall = tp / (tp+fn) if (tp+fn) else 0.0
-            f1 = 2*precision*recall / (precision+recall) if (precision+recall) else 0.0
-            avg_val_loss = val_loss / len(val_loader)
-            tqdm.write(f"Validation -> Loss: {avg_val_loss:.4f}, Acc: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}\n ------------------")
+        accuracy = (tp+tn) / (tp+fp+tn+fn)
+        precision = tp / (tp+fp) if (tp+fp) else 0.0
+        recall = tp / (tp+fn) if (tp+fn) else 0.0
+        f1 = 2*precision*recall / \
+            (precision+recall) if (precision+recall) else 0.0
+        avg_val_loss = val_loss / len(val_loader)
+        tqdm.write(
+            f"Validation -> Loss: {avg_val_loss:.4f}, Acc: {accuracy:.4f}, Precision: {precision:.4f}, Recall: {recall:.4f}, F1: {f1:.4f}\n ------------------")
 
     return avg_val_loss, accuracy, precision, recall, f1
 
 
 def save_checkpoint(model, avg_val_loss, least_val_loss, checkpoint_dir, epoch):
-    if(avg_val_loss < least_val_loss):
+    if (avg_val_loss < least_val_loss):
         least_val_loss = avg_val_loss
-        torch.save(model.state_dict(), f"{checkpoint_dir}/least_val_loss_model.pth")
+        torch.save(model.state_dict(),
+                   f"{checkpoint_dir}/least_val_loss_model.pth")
         tqdm.write(f"Best model saved -> epoch: {epoch+1}.")
     return least_val_loss
 
@@ -167,20 +179,20 @@ def main():
 
     in_channels = 1 if args.grayscale else 3
     model = build_model(
-            alpha=args.alpha,
-            in_channels=in_channels,
-            num_512_blocks=args.num_512_blocks)
+        alpha=args.alpha,
+        in_channels=in_channels,
+        num_512_blocks=args.num_512_blocks)
 
     _, train_losses, val_losses, val_accuracies, val_precisions, val_recalls, val_f1s = train(
-                                                                                                        model=model,
-                                                                                                        train_loader=train_loader,
-                                                                                                        val_loader=val_loader,
-                                                                                                        device=device,
-                                                                                                        num_epochs=args.epochs,
-                                                                                                        learning_rate=args.lr,
-                                                                                                        l2_value=args.weight_decay,
-                                                                                                        threshold=args.threshold,
-                                                                                                        checkpoint_dir=args.checkpoint_dir)
+        model=model,
+        train_loader=train_loader,
+        val_loader=val_loader,
+        device=device,
+        num_epochs=args.epochs,
+        learning_rate=args.lr,
+        l2_value=args.weight_decay,
+        threshold=args.threshold,
+        checkpoint_dir=args.checkpoint_dir)
 
 
 if __name__ == "__main__":
